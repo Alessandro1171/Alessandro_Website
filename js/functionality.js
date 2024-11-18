f4db4220424858f91370c14fc32f9284682b135e
Alessandro Dare <alessdare@gmail.com>
2024-11-18 15:09:10 -0500

    added contact, footer, and experience/education sections

--- js/functionality.js
+++ js/functionality.js
@@ -32,3 +32,44 @@ function projectItemDetails(projectItem){
     document.querySelector(".pp_thumbmail img").src = projectItem.querySelector(".project_img").src;
     document.querySelector(".project_popup-body").innerHTML = projectItem.querySelector("project_item-details").innerHTML;
 }
+
+
+/*Input function*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc(){
+    let parent  =this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) =>{
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+
+/*Srcoll Section Functionality*/
+const sections = documment.querySelectorAll("section[id]");
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter(){
+    let scrollY = window.scrollY;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop -50,
+        sectionId = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".navigation a[href*="+sectionId+"]").classList.add("active-link")
+        }
+        else{
+            document.querySelector(".navigation a[href*="+sectionId+"]").classList.remove("active-link")
+        }
+    })
+}
